41dc4ff02ad51afdb35abd5602f3792d553e523f
Joao Galeao <jbgaleao@gmail.com>
2023-06-20 23:43:43 -0300

    Adding the MoviesController

---
+++ eTickets/Controllers/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using eTickets.Data;
+using eTickets.Models;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace eTickets.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MoviesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Movie> allMovies = await _context.MOVIES.ToListAsync();
+            return View();
+        }
+    }
+}
